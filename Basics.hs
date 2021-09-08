650c64934609a65a14f0c46513bc82089410694a
Chris Watson <ccwatson@seas.upenn.edu>
2021-09-07 20:17:07 -0400

    Finished Basics.hs

--- Basics.hs
+++ Basics.hs
@@ -62,6 +62,7 @@ Try clicking on "Evaluate..." below.
 -}
 
 -- >>> 3 * (4 + 5)
+-- 27
 
 {-
 A Haskell module (like this one) is a list of *definitions*. These definitions
@@ -76,6 +77,7 @@ We can ask VSCode to calculate these values, just as we did above.
 -}
 
 -- >>> ex
+-- 27
 
 {-
 Whenever we give a name to an expression, it is a good idea to also write down
@@ -104,12 +106,14 @@ Compare the value of a extra-large `Integer`
 -}
 
 -- >>> bigInteger
+-- 12345678901234567890
 
 {-
 with an `Int`
 -}
 
 -- >>> bigInt
+-- -6101065172474983726
 
 {-
 Above, we declared the type of an expression separately from giving it a
@@ -118,6 +122,7 @@ still annotate it with its type using `::`.
 -}
 
 -- >>> 31 * (42 + 56) :: Integer
+-- 3038
 
 {-
 More generally, the type annotation can be attached to any subexpression, not
@@ -125,6 +130,7 @@ just at the top level.
 -}
 
 -- >>> (31 :: Integer) * (42 + 56)
+-- 3038
 
 {-
 It is good style to annotate the type of *every* declaration in a Haskell
@@ -149,20 +155,26 @@ using the same overloaded syntax.
 -}
 
 -- >>> 31 * (42 + 56) :: Double    -- double precision floating point
+-- 3038.0
 
 {-
 Furthermore, you'll also find characters, strings and boolean values.
 -}
 
 -- >>> 'a' :: Char                 -- characters
+-- 'a'
 
 -- >>> "abcd" :: String            -- strings
+-- "abcd"
 
 -- >>> "cis" ++ "552"              -- string concatenation
+-- "cis552"
 
 -- >>> True :: Bool                -- boolean values
+-- True
 
 -- >>> 1 <= 3 || False && 3 > 2    -- boolean operators, comparisons
+-- True
 
 {-
 What is a little different about Haskell is that everything is an expression,
@@ -171,6 +183,7 @@ expressions.
 -}
 
 -- >>> (if ex > 28 then 1 else 0) + 2 :: Int
+-- 2
 
 {-
 Now the last basic type, shown below, is subtle. It is a special constant,
@@ -181,6 +194,7 @@ this basic type is that there is only *one* value with type `()`.
 -}
 
 -- >>> () :: ()            -- 'unit' (both value and type have the same syntax)
+-- ()
 
 {-
 What is Abstraction?
@@ -210,12 +224,14 @@ We call functions by providing them with arguments.
 -}
 
 -- >>> pat 31 42 56
+-- 3038
 
 {-
 No parentheses are necessary, unless the argument itself is a compound expression.
 -}
 
 -- >>> pat (30 + 1) 42 56
+-- 3038
 
 {-
 The important question is not "What does this function do?"
@@ -700,7 +716,8 @@ error if it is ever evaluated.
 -}
 
 jn' :: Maybe (Maybe a) -> Maybe a
-jn' = undefined
+jn' (Just x) = x
+jn' Nothing = Nothing
 
 {-
 'Maybe' is useful for partial functions
@@ -804,14 +821,17 @@ Try evaluating `c1` and `c2`.
 -}
 
 -- >>> c1
+-- [True,False,False]
 --
 -- >>> c2
+-- [1]
 --
 
 {-
 And check out the type of `c3`.
 -}
 
+c3 :: [[a]]
 c3 = [] : []
 
 {-
@@ -838,8 +858,10 @@ Try evaluating `s1` and `s2`.
 -}
 
 -- >>> s1
+-- "abc"
 --
 -- >>> s2
+-- "abc"
 --
 
 {-
@@ -956,7 +978,7 @@ range :: Int -> Int -> [Int]
 **Step 3**: Define the function. This part is for you to do for your quiz.
 -}
 
-range i j = undefined
+range i j = if i > j then [] else i : range (i + 1) j
 
 {-
 **Step 4**: Run the tests.
@@ -1004,7 +1026,8 @@ lists that have three or more elements.
 -}
 
 isLong :: [a] -> Bool
-isLong = undefined
+isLong (_ : _ : _ : _) = True
+isLong _ = False
 
 testIsLong :: Test
 testIsLong =
@@ -1094,8 +1117,8 @@ listIncr :: [Int] -> [Int]
 {-
 **Step 3**: Define the function.
 -}
-
-listIncr = undefined
+listIncr [] = []
+listIncr (x : xs) = (x + 1) : listIncr xs
 
 {-
 **Step 4**: Run the tests.
